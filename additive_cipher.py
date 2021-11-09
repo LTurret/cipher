4d55d824981549adeb625a29c80cc05c84c72952
guantou <mole30608@gmail.com>
2021-11-09 12:54:52 +0800

    Create additive_cipher.py

---
+++ additive_cipher.py
@@ -0,0 +1,17 @@
+def AdditiveCipherEncrypto(P,Key):
+  C=''
+  for i in range(len(P)):
+    if P[i].isalpha():
+      C+=chr((ord(P[i])-ord('a')+Key)%26+ord('a'))
+    else:
+      C+=' '
+   return C
+
+def AdditiveCipherDecrypto(C,Key):
+  P=''
+  for i in range(len(C)):
+    if C[i].isalpha():
+      P+=chr((ord(C[i])-ord('a')+Key)%26+ord('a'))
+    else:
+      P+=' '
+   return P
