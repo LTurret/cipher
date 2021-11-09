8d7eea4c990af8524999b8de287ab95e2fdf0623
guantou <mole30608@gmail.com>
2021-11-09 12:57:40 +0800

    Create multiplicative_cipher.py

---
+++ multiplicative_cipher.py
@@ -0,0 +1,21 @@
+def MultiplicativeCipherEncrypto(P,Key):
+  C=''
+  for i in range(len(P)):
+    if P[i].isalpha():
+      C+=chr((ord(P[i])-ord('a')*Key)%26+ord('a'))
+    else:
+      C+=' '
+  return C
+
+def MultiplicativeCipherDecrypto(C,Key):
+  P=''
+  ReverseKey=0
+  for j in range(26):
+    if Key*j%26==1:
+      ReverseKey=j
+  for i in range(len(C)):
+    if C[i].isalpha():
+      P+=chr((ord(C[i])-ord('a')*ReverseKey)%26+ord('a'))
+    else:
+      P+=' '
+  return P
