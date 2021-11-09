0d0739a7433ba7772e653e8565df481f4084e691
guantou <mole30608@gmail.com>
2021-11-09 17:27:09 +0800

    Update vigenere_cipher.py

--- vigenere_cipher.py
+++ vigenere_cipher.py
@@ -1,21 +1,35 @@
-def VigenereCipherEncrypto(P, Key):
+def vigenere_encrypt(P, Key):
+    Key=Key.lower()
     C = ""
     count = 0
     for i in range(len(P)):
-        if P[i].isalpha():
-            C += chr((ord(P[i])-ord("a")+ord(Key[count % len(Key)])) % 26+ord("a"))
+        if P[i].isalpha() and P[i].islower():
+            C += chr((ord(P[i])-ord("a")*2 +
+                     ord(Key[count % len(Key)])) % 26+ord("a"))
+            count += 1
+        elif P[i].isalpha() and P[i].isupper():
+            C += chr((ord(P[i])-ord("A")-ord("a") +
+                     ord(Key[count % len(Key)])) % 26+ord("A"))
             count += 1
         else:
             C += " "
     return C
 
-def VigenereCipherDecrypto(C, Key):
+
+def vigenere_decrypt(C, Key):
+    Key=Key.lower()
     P = ""
     count = 0
     for i in range(len(C)):
-        if C[i].isalpha():
-            P += chr((ord(C[i])-ord("a")-ord(Key[count % len(Key)])) % 26+ord("a"))
+        if C[i].isalpha() and C[i].islower():
+            P += chr((ord(C[i])-
+                     ord(Key[count % len(Key)])) % 26+ord("a"))
+            count += 1
+        elif C[i].isalpha() and C[i].isupper():
+            P += chr((ord(C[i])-
+                     ord(Key[count % len(Key)])) % 26+ord("A"))
             count += 1
         else:
             P += " "
     return P
+
