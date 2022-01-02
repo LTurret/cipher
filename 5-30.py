053d3adce190b8c39d870aeb23196964815a0434
guantou <mole30608@gmail.com>
2022-01-02 21:28:52 +0800

    Create 5-30.py

---
+++ 5-30.py
@@ -0,0 +1,11 @@
+seed = '1110'
+
+for i in range(20):
+    b0 = int(seed[3])
+    b1 = int(seed[2])
+    b2 = int(seed[1])
+    b3 = int(seed[0])
+    b4 = b1 ^ b0
+    seed = f'{b4}{b3}{b2}{b1}{b0}'
+    ki = b0
+    print(ki,end='')
