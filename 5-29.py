4ba0d1bf85973e80ba47c9147258065dd305c5ca
guantou <mole30608@gmail.com>
2022-01-02 21:28:36 +0800

    Create 5-29.py

---
+++ 5-29.py
@@ -0,0 +1,15 @@
+seed='0001'
+seedset=set()
+for i in range(20):
+    b0=int(seed[3])
+    b1=int(seed[2])
+    b2 = int(seed[1])
+    b3 = int(seed[0])
+    b4 = b2^b0^b3
+    seed = f'{b4}{b3}{b2}{b1}{b0}'
+    ki=b0
+    print(f'round {i}|{seed}|{ki}')
+    seedset.add(seed)
+
+print("period:",len(seedset))
+
